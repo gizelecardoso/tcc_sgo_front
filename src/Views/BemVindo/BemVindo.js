e64bd2e536927597410789445f50d914ce136de2
Gizele Santos <gizele.ccardoso@gmail.com>
2021-03-21 21:27:44 -0300

    Alterações na tela de BemVindo - somente o estilo

--- src/Views/BemVindo/BemVindo.js
+++ src/Views/BemVindo/BemVindo.js
@@ -1,20 +1,12 @@
-import React, { Fragment } from "react";
-import { Button, TextInput, View, Text, TouchableOpacity} from "react-native";
-import estilo from "./estilo.js"
-import { FontAwesome5 } from "@expo/vector-icons";
-import { Entypo } from '@expo/vector-icons'; 
-import { AppBar } from "@material-ui/core";
-import { Appbar } from "react-native-paper";
-import { DrawerNavigator } from "../../DrawerNavigator.js";
+import React, { Fragment } from 'react';
 import Cabecalho from "../../Components/Cabecalho/Cabecalho.js";
 
 const BemVindo = (props) => {
-    return(
-        <Fragment>
-            <Cabecalho navigation={props.navigation} name={props.route.params.name_official} login={props.route.params.login_name}/>
-        </Fragment>
-    );
-
+	return (
+		<Fragment>
+			<Cabecalho navigation={props.navigation} name={props.route.params.name_official} login={props.route.params.login_name} />
+		</Fragment>
+	);
 }
 
 export default BemVindo;
