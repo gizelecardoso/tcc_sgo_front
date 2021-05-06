f7cc652bde08f4fcbc283297819304393ce20279
Gizele Santos <gizele.ccardoso@gmail.com>
2021-05-05 21:14:35 -0300

    activity: Adicionar datas inicial/final/parada na alteracao dos status - Iniciar validaçao de funcionarios nao vinculados a atividades

--- src/services/api/Activity/update_api.js
+++ src/services/api/Activity/update_api.js
@@ -2,8 +2,9 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import { Platform } from "react-native";
 import moment from "moment";
 
-const updateActivity = async (values, id, status) => {
+const updateActivity = async (values, id, status, date) => {
     let url = 'localhost';
+
     if(Platform.OS == 'android'){
         url = '10.0.2.2';
     }
@@ -11,6 +12,21 @@ const updateActivity = async (values, id, status) => {
     const format_date_front_to_back = (date_api) => {
 		return moment(date_api).format();
 	}
+
+    if (status == 'executando'){
+        console.log('data inicial');
+        values.initialDate = format_date_front_to_back(date);
+        console.log(values.initialDate);
+    } else if (status == 'finalizada') {
+        console.log('data final');
+        values.finalDate = format_date_front_to_back(date);
+        console.log(values.finalDate);
+    } else if (status == 'pausada') {
+        console.log('data parada');
+        values.stoppedDate = format_date_front_to_back(date);
+        console.log(values.stoppedDate);
+    }
+
 
     const response = await fetch(`http://${url}:3000/activities/${id}`, {
         method: 'PUT',
@@ -26,7 +42,10 @@ const updateActivity = async (values, id, status) => {
             expected_initial_date: format_date_front_to_back(values.expectedInitialDate),
             expected_final_date: format_date_front_to_back(values.expectedFinalDate),
             activity_status: status,
-            official_id: values.officialId
+            official_id: values.officialId,
+            initial_date: values.initialDate,
+            final_date: values.finalDate,
+            stopped_date: values.stoppedDate,
         })
     });
 
